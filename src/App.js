c5abb8884f3547919ca2824b8ecf69aadeb0b1e9
ngodi albert <albertngodi@gmail.com>
2020-04-25 02:27:29 +0100

    implemented items preview

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 import {Route, Switch} from 'react-router-dom';
 
 import HomePage from './pages/Homepage/HomePage';
+import ShopPage from './pages/Shop/ShopPage';
+
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -17,4 +17,5 @@ const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => {
     );
 }
 
+
 export default withRouter(MenuItem);
