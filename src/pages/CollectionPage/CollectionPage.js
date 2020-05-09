20b1cb3ff005458e2f7667ef2204ffa21af773c8
ngodi albert <albertngodi@gmail.com>
2020-05-09 21:46:00 +0100

    setup localstorage with redux-persist

---
+++ src/pages/CollectionPage/CollectionPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import CollectionItem from '../../components/CollectionItem/CollectionItem';
+
+import './CollectionPage.scss';
+
+const CollectionPage = ( { match}) => {
+    return (
+        <div className="collection">
+            <h2>COLLECTION PAGE</h2>
+        </div>
+    );
+};
+
+export default CollectionPage
