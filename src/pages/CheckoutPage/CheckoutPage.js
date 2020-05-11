7ffb25c9b3680f0126bc11f5366744608dab178c
ngodi albert <albertngodi@gmail.com>
2020-05-11 02:25:46 +0100

    stripe checkout finalized

--- src/pages/CheckoutPage/CheckoutPage.js
+++ src/pages/CheckoutPage/CheckoutPage.js
@@ -5,6 +5,7 @@ import { createStructuredSelector } from 'reselect';
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
 import CheckoutItem from '../../components/CheckoutItem/CheckoutItem';
+import StripeButton from '../../components/StripeButton/StripeButton';
 
 import './CheckoutPage.scss';
 
@@ -35,6 +36,11 @@ const CheckoutPage = ({ cartItems, total}) => (
       <div className="total">
           <span>TOTAL: ${total}</span>
       </div>
+      <div className="test-warning">
+          **Please use the following test credit card for payment with this Demo version**<br />
+          4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+      </div>
+      <StripeButton price = {total}/>
     </div>
 )
 
